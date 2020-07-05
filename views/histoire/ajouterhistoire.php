e75526ddca51d95d1e8ed22061c559794d5cafef
Papibadiste <labussieremailpro@gmail.com>
2020-07-05 19:46:28 +0200

    visu de ajouthistoire

--- views/histoire/ajouterhistoire.php
+++ views/histoire/ajouterhistoire.php
@@ -27,23 +27,57 @@
                 <div>
                     <form method="post" action="">
                         <div class="form-group">
+                            <div class="row">
+                                <div class="col-sm-6 col-12">
+                                    <label class="ligne" for="titre">Titre:</label>
+                                    <input class="ligne" type="text" name="titre" id="titre">
+                                </div>
+                                <div class="col-sm-6 col-12">
+                                <label class="ligne" for="categorie">Categorie:</label>
+                                    <select class="form-control-sm ligne" name="categorie" id="categorie">
+                                        <option>Default select</option>
+                                        <option>Default select</option>
+                                        <option>Default select</option>
+                                        <option>Default select</option>
+                                        <option>Default select</option>
+                                        <option>Default select</option>
+                                        <option>Default select</option>
+                                        <option>Default select</option>
+                                        <option>Default select</option>
+                                    </select>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="form-group">
                             <h3>Paragraphe1</h3>
-                            <label for="Paragraphe1">Texte du paragraphe1 (??? caractere max:</label>
-                            <textarea name="" id="" cols="30" rows="10"></textarea>
+                            <label class="ligne" for="Paragraphe1">Texte du paragraphe1 (??? caractere max):</label> <br>
+                            <textarea class="ligne" name="Paragraphe1" id="Paragraphe1" rows="8"></textarea>
+                            <label for="photop1">Choisir une image</label>
+                            <input type="file" class="form-control-file" id="photop1" name="photop1">
 
                         </div>
 
                         <div class="form-group">
                             <h3>Paragraphe2</h3>
-                            <label  for="">Texte du paragraphe2 (??? caractere max):</label> <br>
-                            <textarea name="" id="" cols="30" rows="10"></textarea>
+                            <label class="ligne" for="Paragraphe2">Texte du paragraphe2 (??? caractere max):</label> <br>
+                            <textarea class="ligne" name="" id="Paragraphe2" rows="8"></textarea>
+                            <label for="photop2">Choisir une image</label>
+                            <input type="file" class="form-control-file" id="photop2" name="photop2">
+
 
                         </div>
                         <div class="form-group">
                             <h3>Paragraphe3</h3>
-                            <label for="Password">Texte du paragraphe3 (??? caractere max):</label>
-                            <textarea name="" id="" cols="30" rows="10"></textarea>
+                            <label class="ligne" for="Paragraphe3">Texte du paragraphe3 (??? caractere max):</label> <br>
+                            <textarea class="ligne" name="" id="Paragraphe3" rows="8"></textarea>
+                            <label for="photop3">Choisir une image</label>
+                            <input type="file" class="form-control-file" id="photop3" name="photop3">
 
+                        </div>
+                        <div class="form-check">
+                            <input type="checkbox" name="chek" class="form-check-input" id="Validation">
+                            <label class="form-check-label" for="Validation">En décidant de m'inscrire j'accepte de partager mon histoire </label>
                         </div>
                         <div class="d-flex justify-content-center">
                             <button type="submit" name="formconnection" class="btn btn-valider">Valider</button>
