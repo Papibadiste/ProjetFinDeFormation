2582139b6c241855d33c21bb366d103476764012
Papibadiste <labussieremailpro@gmail.com>
2020-07-11 20:34:05 +0200

    mise en place du .gitignore

---
+++ config/base_url_sample.php
@@ -0,0 +1,5 @@
+<?php 
+
+define('BASE_URL', '/url/');
+
+$requestUri = str_replace('/url/', '', $_SERVER['REQUEST_URI']);
--- config/bdd.php
+++
@@ -1,5 +0,0 @@
-<?php
-    define('DB_HOST', 'localhost');
-    define('DB_NAME', 'projet_final');
-    define('DB_USER', 'root');
-    define('DB_PASSWORD', '');
